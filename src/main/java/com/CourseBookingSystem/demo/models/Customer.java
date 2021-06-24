e2ae200ba093c655becda0452d40c86e1c42be3a
rock-007 <umair.ashraf2019@gmail.com>
2021-06-24 10:48:24 +0100

    init commit

---
+++ src/main/java/com/CourseBookingSystem/demo/models/Customer.java
@@ -0,0 +1,43 @@
+package com.CourseBookingSystem.demo.models;
+
+public class Customer {
+
+    //    private Long id;
+    private String name;
+    private String town;
+    private int age;
+
+
+    public Customer(String name, String town, int age) {
+        this.name = name;
+        this.town = town;
+        this.age = age;
+    }
+
+    public Customer() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTown() {
+        return town;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
